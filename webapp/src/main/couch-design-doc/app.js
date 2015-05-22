d40c3e5ca29991ce9c5a503ac0c2844abc1b721e
Alex Anderson <anderson@medicmobile.org>
2015-05-22 14:04:18 +0200

    Sanitise article content before creating title for use in view

--- webapp/src/main/couch-design-doc/app.js
+++ webapp/src/main/couch-design-doc/app.js
@@ -12,10 +12,38 @@ module.exports = d;
 
 d.views.articles = {
   map: function(doc) {
+    // From: https://stackoverflow.com/questions/295566/sanitize-rewrite-html-on-the-client-side/430240#430240
+    var sanitizer = (function() {
+      var tagBody = '(?:[^"\'>]|"[^"]*"|\'[^\']*\')*';
+
+      var tagOrComment = new RegExp(
+          '<(?:'
+          // Comment body.
+          + '!--(?:(?:-*[^->])*--+|-?)'
+          // Special "raw text" elements whose content should be elided.
+          + '|script\\b' + tagBody + '>[\\s\\S]*?</script\\s*'
+          + '|style\\b' + tagBody + '>[\\s\\S]*?</style\\s*'
+          // Regular name
+          + '|/?[a-z]'
+          + tagBody
+          + ')>',
+          'gi');
+      function removeTags(html) {
+        var oldHtml;
+        do {
+          oldHtml = html;
+          html = html.replace(tagOrComment, '');
+        } while (html !== oldHtml);
+        return html.replace(/</g, '&lt;');
+      }
+
+      return { sanitize:removeTags };
+    }());
     if(doc.type === 'article') {
-      var title = doc.content.length > 100 ?
-        doc.content.substring(0, 99) + "…" :
-        doc.content;
+      var cleanContent = sanitizer.sanitize(doc.content);
+      var title = cleanContent.length > 100 ?
+        cleanContent.substring(0, 99) + "…" :
+        cleanContent;
       emit(doc._id, title);
     }
   }
